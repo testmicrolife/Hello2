58c6ef00eb57598247e39854de1ea03600ec0178
wayne <wayne.chen@microlife.com.tw>
2013-03-08 14:26:12 +0800

    test update ma,hava
    Signed-off-by: wayne <wayne.chen@microlife.com.tw>

--- src/com/example/hello2/MainActivity.java
+++ src/com/example/hello2/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i=1;
+        int y=3;
     }
 
 
