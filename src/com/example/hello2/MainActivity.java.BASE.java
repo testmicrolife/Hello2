46129bdb6a47f44dcc07db9a194047846f1d0ebd
wayne <wayne.chen@microlife.com.tw>
2013-03-15 11:19:13 +0800

    33
    Signed-off-by: wayne <wayne.chen@microlife.com.tw>

--- src/com/example/hello2/MainActivity.java.BASE.java
+++ src/com/example/hello2/MainActivity.java.BASE.java
@@ -10,15 +10,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a=1;
-        int b=2;
-        int c=3;
-        int d=4;
-        int e=5;
-        int f=6;        
+        int i=1;
+        int y=3;
+        int x=4;
+        int k=5;
+        int z=6;
         int g=7;
-        int h=8;
-        int i=9;
     }
 
 
